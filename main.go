d8d7d0e6e80992389037c77002a1f6d9b62f93ee
Jonas Riedel <jriedel@united-internet.de>
2023-03-03 15:40:05 +0100

    implement converting to csv and don't use deprecated stuff

--- main.go
+++ main.go
@@ -1,30 +1,84 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
+	"flag"
 	"fmt"
-	"io/ioutil"
+	"os"
+	"reflect"
+	"strings"
 )
 
-type Person struct {
-	Name    string `json:"name"`
-	Age     int    `json:"age"`
-	Address string `json:"address"`
-}
-
 func main() {
-	jsonData, err := ioutil.ReadFile("people.json")
+	// Define command line flags
+	jsonFile := flag.String("json", "people.json", "the path to the input JSON file")
+	csvFile := flag.String("csv", "people.csv", "the path to the output CSV file")
+	flag.Parse()
+
+	// Validate command line arguments
+	if *jsonFile == "" {
+		fmt.Println("Error: You must specify an input JSON file with the -json flag")
+		return
+	}
+	if *csvFile == "" {
+		fmt.Println("Error: You must specify an output CSV file with the -csv flag")
+		return
+	}
+
+	// Read the JSON data from the file
+	jsonData, err := os.ReadFile(*jsonFile)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	var people []Person
-	err = json.Unmarshal(jsonData, &people)
+	// Unmarshal the JSON data into a slice of maps
+	var data []map[string]interface{}
+	err = json.Unmarshal(jsonData, &data)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(people)
+	// Extract the header row from the data
+	header := make([]string, 0)
+	for k := range data[0] {
+		header = append(header, k)
+	}
+
+	// Convert the data to a 2D slice of strings
+	rows := make([][]string, len(data)+1)
+	rows[0] = header
+	for i, d := range data {
+		row := make([]string, len(header))
+		for j, h := range header {
+			if reflect.TypeOf(d[h]).Kind() == reflect.Slice {
+				// If the value is a slice, join it with commas
+				row[j] = strings.Join(d[h].([]string), ", ")
+			} else {
+				// Otherwise, just use the string representation of the value
+				row[j] = fmt.Sprintf("%v", d[h])
+			}
+		}
+		rows[i+1] = row
+	}
+
+	// Write the data to the CSV file
+	file, err := os.Create(*csvFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, row := range rows {
+		err := writer.Write(row)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
